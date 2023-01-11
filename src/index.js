aa26f576684c6bd2381fde205ca807a6b166beb1
MaxT6 <maximiliant6@gmail.com>
2023-01-11 13:51:36 -0500

    added Redux store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// REDUX IMPORTS
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './state/reducer';
+
+// REDUX STORE
+let store
+export const resetStore = () => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+};
+resetStore();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
